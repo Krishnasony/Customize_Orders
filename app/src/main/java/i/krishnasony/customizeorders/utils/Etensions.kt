4e5d1a0fc7f934eda4ffd2b740a49a61630b3633
Krishnasony <krishna.sony@talview.com>
2020-01-26 18:08:34 +0530

    extension function for livedata and getting double in rupee format

--- app/src/main/java/i/krishnasony/customizeorders/utils/Etensions.kt
+++ app/src/main/java/i/krishnasony/customizeorders/utils/Etensions.kt
@@ -1,2 +1,21 @@
 package i.krishnasony.customizeorders.utils
 
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
+import java.text.NumberFormat
+import java.util.*
+
+fun <T> LiveData<T>.observeOnce(lifecycleOwner: LifecycleOwner, observer: Observer<T>) {
+    observe(lifecycleOwner, object : Observer<T> {
+        override fun onChanged(t: T?) {
+            observer.onChanged(t)
+            removeObserver(this)
+        }
+    })
+}
+
+fun Double.getRupeeFormat():String{
+    val format = NumberFormat.getCurrencyInstance(Locale("en", "in"))
+    return format.format(this)
+}
